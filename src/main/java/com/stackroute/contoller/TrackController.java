82f0f5730da2516475687bdf9e4e9aa791d90141
amrit96pal <46045121+amrit96pal@users.noreply.github.com>
2019-01-24 00:39:09 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/contoller/TrackController.java
+++ src/main/java/com/stackroute/contoller/TrackController.java
@@ -36,18 +36,12 @@ public class TrackController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @PostMapping("perform")
+    @PostMapping("track")
     public ResponseEntity<?> insertTrack(@RequestBody Track track) throws TrackAlreadyExistsException {
         ResponseEntity responseEntity;
         trackService.saveTheTrack(track);
         responseEntity=new ResponseEntity<String>("Success-track stored", HttpStatus.CREATED);
         return responseEntity;
-        /*try{
-
-        }catch (Exception e){
-            responseEntity=new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
-            return responseEntity;
-        }*/
     }
 
     //GetAll tracks in the databases
@@ -58,17 +52,11 @@ public class TrackController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @GetMapping("perform")
+    @GetMapping("track")
     public ResponseEntity<?> getAllTracks() throws TrackAlreadyExistsException {
         ResponseEntity responseEntity;
         responseEntity= new ResponseEntity<List<Track>>(trackService.returnAllTracks(),HttpStatus.ACCEPTED);
         return responseEntity;
-/*        try {
-
-        }catch (Exception e){
-            responseEntity= new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
-            return responseEntity;
-        }*/
     }
 
     //Update the track if already exists
@@ -79,18 +67,12 @@ public class TrackController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @PutMapping("perform")
+    @PutMapping("track")
     public ResponseEntity<?> updateTracks(@RequestParam("trackid") int trackid,@RequestParam("comments") String comments) throws TrackNotFoundExeption {
         ResponseEntity responseEntity;
         trackService.updateTrack(trackid,comments);
         responseEntity= new ResponseEntity<String>("Succees-track updated",HttpStatus.OK);
         return responseEntity;
-       /* try {
-
-        }catch (Exception e){
-            responseEntity= new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
-            return responseEntity;
-        }*/
     }
 
     //Delete the track
@@ -101,19 +83,11 @@ public class TrackController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @DeleteMapping("perform")
+    @DeleteMapping("track")
     public ResponseEntity<?> deleteTrack(@RequestParam int trackid) throws TrackNotFoundExeption {
         trackService.deletTrack(trackid);
         ResponseEntity responseEntity= new ResponseEntity<String>("Success-Track deleted",HttpStatus.ACCEPTED);
         return responseEntity;
-       /* try{
-            trackService.deletTrack(trackid);
-            responseEntity=new ResponseEntity<String>("Success-Track deleted",HttpStatus.CREATED);
-            return responseEntity;
-        }catch (Exception e){
-            responseEntity= new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
-            return responseEntity;
-        }*/
     }
 
     //Find the track by trackname
@@ -124,18 +98,10 @@ public class TrackController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
-    @GetMapping("performs")
+    @GetMapping("tracks")
     public ResponseEntity<?> findTrackByName(@RequestParam String trackname) throws TrackNotFoundExeption {
         ResponseEntity responseEntity;
         responseEntity= new ResponseEntity<List<Track>>(trackService.getByTrackNAme(trackname),HttpStatus.FOUND);
         return responseEntity;
-/*        try {
-
-        }catch (Exception e){
-            responseEntity=new ResponseEntity<String>(e.getMessage(),HttpStatus.CONFLICT);
-            return responseEntity;
-        }
-    }*/
-
     }
 }
